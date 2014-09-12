9ad36f67e9d779c90377902c502bd776a31d508d
Warren <warren.ali@gmail.com>
2014-09-12 11:17:00 +0100

    refactor / data profile fix

--- data_profile.py
+++ data_profile.py
@@ -1,5 +1,8 @@
 from time import strftime
 from utils.data_profiler import node_summary as nodes
+from utils.data_profiler import document_summary
+from utils.data_profiler import network_centrality
+from utils.data_profiler import central_nodes
 
 
 current_time = strftime("%Y-%m-%d %H:%M:%S")
@@ -15,12 +18,12 @@ def get_profile():
     #terms.get_term_stats()
     #names = np_summary.NounPhraseSummary(verbose=True)
     #names.get_np_stats()
-    #documents = document_summary.DocumentsSummary(verbose=False)
-    #documents.get_document_stats()
-    #structure = network_centrality.InOutDegree()
-    #structure.show_degrees()
-    #centre = central_nodes.CentralNodes()
-    #centre.show_nodes()
+    documents = document_summary.DocumentsSummary(verbose=False)
+    documents.get_document_stats()
+    structure = network_centrality.InOutDegree()
+    structure.show_degrees()
+    centre = central_nodes.CentralNodes()
+    centre.show_nodes()
     #sentence_summary = sentence.SentenceNodes()
     #sentence_summary.show_counts()
     #stats = document_metrics.PopulationStatistics(models, text_io, sem=semantic_tools)
