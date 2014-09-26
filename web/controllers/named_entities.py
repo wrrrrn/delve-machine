550b3ca100de00a9c4afc28f79a690dff68a0e4a
Warren <warren.ali@gmail.com>
2014-09-26 13:12:57 +0100

    named entities controller

--- web/controllers/named_entities.py
+++ web/controllers/named_entities.py
@@ -7,24 +7,26 @@ class NamedEntityController:
         self.noun_phrase = name
         self.n = models.NounPhrase(self.noun_phrase)
         self.n.fetch()
+        self.exists = self.n.exists
+        self.exclude = ['Named Entity', 'Noun Phrase']
         self.properties = {}
 
     def get_properties(self):
         if self.n.exists:
+            node_labels = self.n.vertex.get_labels()
+            labels = [l for l in node_labels if l not in self.exclude]
             self.properties["node_type"] = "NamedEntity"
-            self.properties["name"] = self.n.vertex["title"]
-            self.properties["content"] = self.n.vertex["content"]
-            self.properties["mentions"] = self.get_mentions()
-            self.properties["domain"] = self.n.vertex["publication"]
+            self.properties["name"] = self.n.vertex["noun_phrase"]
+            if len(labels) > 0:
+                self.properties["has_labels"] = True
+                self.properties["labels"] = labels
+                if "Member of Parliament" in labels:
+                    self.get_mp_properties(self.n.vertex)
             return self.properties
         else:
             return None
 
-    def get_mentions(self):
-        names = self.n.get_mentions("Named Entity")
-        terms = self.n.get_mentions("Unique Term")
-        n = [{"type": "name", "value": n["noun_phrase"]} for n in names]
-        t = [{"type": "term", "value": t["term"]} for t in terms]
-        for mention in n:
-            yield mention
+    def get_mp_properties(self, node):
+        self.properties["party"] = node["party"]
+
 
