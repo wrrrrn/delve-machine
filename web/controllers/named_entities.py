bbf86d8160ae9305ff2d59912760a48dd404da42
Warren <warren.ali@gmail.com>
2014-09-29 11:29:47 +0100

    controller implementation

--- web/controllers/named_entities.py
+++ web/controllers/named_entities.py
@@ -11,32 +11,36 @@ class NamedEntityController:
         self.exists = self.n.exists
         self.node_type = "NamedEntity"
         self.name = ""
+        self.outgoing = []
+        self.incoming = []
+        self._is_mp = False
+        self._set_properties()
 
     def _set_properties(self):
         if self.n.exists:
-            self.name = self.n.vertex["noun_phrase"]
             self._get_node_properties()
-            node_labels = self.n.vertex.get_labels()
-            self.labels = [l for l in node_labels if l not in self.exclude]
-            if "Member of Parliament" in self.labels:
-                self.get_mp_properties(self.n.vertex)
+            self.name = self._properties["name"]
+            self.labels = self._properties["labels"]
 
     def labels(self):
         for label in self.labels:
             yield label
 
     def is_mentioned_in(self):
-        if "document_count" in self._properties:
-            for doc in self._properties["documents"]:
-                yield doc
+        for doc in self._properties["documents"]:
+            yield doc
 
     def is_associated_with(self):
-        if "document_count" in self._properties:
-            for doc in self._properties["documents"]:
-                yield doc
+        for rel in self._properties["outgoing"]:
+            if rel[0] == "IS_ASSOCIATED_WITH":
+                yield rel
 
     def _get_node_properties(self):
         stats = [x for x in self.n.get_stats()]
+        self._properties["name"] = self.n.vertex["noun_phrase"]
+        self._properties["labels"] = [
+            l for l in self.n.vertex.get_labels() if l not in self.exclude
+        ]
         self._properties["sentence_count"] = stats[0]
         self._properties["document_count"] = stats[1]
         self._properties["term_count"] = stats[2]
@@ -48,19 +52,11 @@ class NamedEntityController:
         ]
         if self._properties["document_count"] > 0:
             self._properties["documents"] = [
-                x['title'] for x in self.n.get_documents()
+                {"title": x['title']} for x in self.n.get_documents()
             ]
-
-        print "documents:"
-        docs = [{"title": x['title']} for x in self.n.get_documents()]
-        print docs
-        print "outgoing:"
-        out = [(x[0], self.get_node_name(x[1])) for x in self.n.get_outgoing()]
-        print out
-        self.get_node_name(out[0][1])
-        print "incoming:"
-        incoming = [(x[0], self.get_node_name(x[1])) for x in self.n.get_incoming()]
-        print incoming
+        if "Member of Parliament" in self._properties["labels"]:
+            self._is_mp = True
+            self.get_mp_properties(self.n.vertex)
 
     def get_mp_properties(self, node):
         self._properties["party"] = node["party"]
@@ -76,4 +72,10 @@ class NamedEntityController:
         elif "title" in node:
             return node["title"]
 
+    def show_properties(self):
+        for prop in self._properties:
+            self._print_out(prop, self._properties[prop])
+
+    def _print_out(self, key, value):
+        print "  %-20s%-15s" % (key, value)
 
