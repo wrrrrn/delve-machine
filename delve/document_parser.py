72f963509a8aa1d281ce8f9b45e4c5e30cd1db77
Warren <warren.ali@gmail.com>
2014-10-07 12:26:03 +0100

    experimental parser test

--- delve/document_parser.py
+++ delve/document_parser.py
@@ -140,12 +140,18 @@ class DocumentParser:
             "named_entity_count": named_entity_unique,
             "unique_term_count": len(self._all_terms),
             "pos_sentiments": len(pos_sentiment),
-            "neg_sentiments": len(neg_sentiment),
-            "sentiment_mean":
-                sum(self._all_sentiment)/len(self._all_sentiment),
-            "subjectivity_mean":
-                sum(self._all_subjectivity)/len(self._all_subjectivity)
+            "neg_sentiments": len(neg_sentiment)
         }
+        if len(self._all_sentiment) > 0:
+            self._semantic_feats["sentiment_mean"] = \
+                sum(self._all_sentiment)/len(self._all_sentiment),
+        else:
+            self._semantic_feats["sentiment_mean"] = 0
+        if len(self._all_subjectivity) > 0:
+            self._semantic_feats["subjectivity_mean"] = \
+                sum(self._all_subjectivity)/len(self._all_subjectivity)
+        else:
+            self._semantic_feats["subjectivity_mean"] = 0
         if self.map_statements:
             noun_phrase_unique = len(
                 self.text_tools.unique_lemmas(self._all_nounphrases)
